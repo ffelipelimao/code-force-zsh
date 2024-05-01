049e3b65d62655e99ba759efe93c776742ea7ccc
Luiz Felipe Limão <lflimao@hotmail.com>
2024-04-30 22:02:50 -0300

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,1 +1,3 @@
 code () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}
+
+alias gotest="go test ./... -coverprofile=coverage.out && go tool cover -html=coverage.out -o coverage.html && open coverage.html"
